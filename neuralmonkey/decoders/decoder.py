fix bandit gradient computation

neuralmonkey/decoders/decoder.py
@@ -144,11 +144,11 @@ class Decoder(object):
         # FIXME add multiple samples
         self.sample_logprobs, self.sample_ids = self.sample_batch()
         log("Sample logprobs:")
-        log(self.sample_logprobs)  # batch_size x sample_size
+        log(self.sample_logprobs)  # timestep-length list of batch_size x sample_size tensors
         self.sample_probs = [tf.exp(lp) for lp in self.sample_logprobs]
 
         log("Sample ids:")
-        log(self.sample_ids)  # batch_size
+        log(self.sample_ids)  # timestep-length list of batch_size x 1
 
         # Summaries
         self._init_summaries()
@@ -156,6 +156,8 @@ class Decoder(object):
         log("Decoder initialized.")
         log("runtime_logprobs:")
         log(self.runtime_logprobs)
+
+        self.rewards = tf.placeholder(tf.float32, [None,1])
 
     @property
     def vocabulary_size(self):
@@ -177,7 +179,7 @@ class Decoder(object):
 
     def sample_batch(self):
         """
-        Sample a target words for the full batch and return its
+        Sample a target words for the full batch and return its ids and
         log probabilities
         :param k:
         :return:
@@ -185,12 +187,27 @@ class Decoder(object):
         sample_ids = []
         sample_logprobs = []
         for p in self.runtime_logprobs:  # time steps
-            sample_id = tf.squeeze(tf.cast(tf.multinomial(p, 1), tf.int32))
-            batch_enum = tf.range(tf.shape(sample_id)[0])
-            indices = tf.pack([batch_enum, sample_id], 1)
-            sample_logprob = tf.gather_nd(p, indices)
+            # with gather_nd
+            # FIXME no gradients implemented yet
+            #sample_id = tf.squeeze(tf.cast(tf.multinomial(p, 1), tf.int32))
+            #batch_enum = tf.range(tf.shape(sample_id)[0])
+            #indices = tf.pack([batch_enum, sample_id], 1)
+            #sample_logprob = tf.gather_nd(p, indices)
+            #sample_ids.append(sample_id)
+            #sample_logprobs.append(sample_logprob)
+
+            # with gather and flattening
+            sample_id = tf.cast(tf.multinomial(p, 1), tf.int32) # batch_size x 1
+            flat_p = tf.reshape(p, [-1])  # batch_size*vocab_size
+            batch_size = tf.shape(sample_id)[0]
+            # add correction to indices because of flattening
+            to_add = tf.reshape(tf.range(0, batch_size * self.vocabulary_size,
+                              self.vocabulary_size), [batch_size, -1])
+            indices = sample_id + to_add
+            sample_logprob = tf.gather(flat_p, indices)  # batch_size x 1
             sample_ids.append(sample_id)
             sample_logprobs.append(sample_logprob)
+
         return sample_logprobs, sample_ids
 
     def sample_singleton(self, k, n):
@@ -413,6 +430,9 @@ class Decoder(object):
                                      max_images=256)
 
         return rnn_outputs, rnn_states, output_logits
+
+    def _set_rewards(self, tensor):
+        self.rewards = tensor
 
     def _init_summaries(self):
         """Initialize the summaries of the decoder

neuralmonkey/learning_utils.py
@@ -11,6 +11,7 @@ from neuralmonkey.dataset import Dataset
 from neuralmonkey.tf_manager import TensorFlowManager
 from neuralmonkey.runners.base_runner import BaseRunner, ExecutionResult
 from neuralmonkey.trainers.generic_bandit_trainer import GenericBanditTrainer
+from neuralmonkey.evaluators.bleu import BLEUEvaluator
 
 # pylint: disable=invalid-name
 Evaluation = Dict[str, float]
@@ -297,7 +298,7 @@ def bandit_training_loop(tf_manager: TensorFlowManager,
         log("Initializing TensorBoard summary writer.")
         tb_writer = tf.train.SummaryWriter(log_directory,
                                            tf_manager.sessions[0].graph)
-        log("TesorBoard writer initialized.")
+        log("TensorBoard writer initialized.")
 
     best_score_epoch = 0
     best_score_batch_no = 0
@@ -315,32 +316,40 @@ def bandit_training_loop(tf_manager: TensorFlowManager,
 
                 step += 1
                 seen_instances += len(batch_dataset)
+                log("dataset_batch:")
+                log(batch_n)
+
+                # sample, compute sample probs, derive sample probs wrt params
+                sampling_result = tf_manager.execute(
+                    # TODO build in bandit sampler here
+                    batch_dataset, [trainer], update=False,  # train=False?
+                    summaries=True)
+                sampled_outputs, sampled_logprobs, reg_cost = \
+                    sampling_result['output']
+
+                log("sampled translations and gold translations")
+                log(zip(sampled_outputs, batch_dataset))
+                # evaluate samples
+                # sample_evaluation = evaluation(evaluators, batch_dataset,
+                #                               runners, None,
+                #                               sampled_outputs)  # losses cannot yet be computed
+                # TODO implement sentence-wise evaluator, e.g. sBLEU, check mixer
+                # TODO use evaluator specified in params
+                # rewards for full batch
+                rewards = BLEUEvaluator.bleu(sampled_outputs, batch_dataset)
+
+                # report sample probability
+                log("sample logprobs:")
+                log(sampled_logprobs)
+
+                # update model with samples and their rewards
+
+                _ = tf_manager.execute(
+                    # trainer somehow needs 2 different executables
+                    batch_dataset, [trainer], update=True, summaries=True
+                )
+
                 if step % logging_period == logging_period - 1:
-                    # sample, compute sample probs, derive sample probs wrt params
-                    sampling_result = tf_manager.execute(  # TODO build in bandit sampler here
-                        batch_dataset, [trainer], update=False,  # train=False?
-                        summaries=True)
-                    sampled_outputs, sampled_logprobs, grad_diff, reg_cost = \
-                        sampling_result
-                    # evaluate samples
-                    sample_evaluation = evaluation(evaluators, batch_dataset,
-                                                   runners, None,
-                                                   sampled_outputs)  # losses cannot yet be computed
-                    # TODO implement sentence-wise evaluator, e.g. sBLEU, check mixer
-
-                    # TODO report sample probability
-
-                    # TODO scale grad_diff by reward
-
-                    # TODO add regularization cost
-
-                    # update model with samples and their rewards
-                    stochastic_gradients = None
-                    trainer._set_gradients(stochastic_gradients)
-
-                    _ = tf_manager.execute(  # trainer somehow needs 2 different executables
-                        batch_dataset, [trainer], update=True, summaries=True
-                        )
 
                     train_results, train_outputs = run_on_dataset(
                         tf_manager, runners, batch_dataset,
@@ -354,9 +363,9 @@ def bandit_training_loop(tf_manager: TensorFlowManager,
                                                seen_instances,
                                                train_results,
                                                train=True)
-                else:
-                    tf_manager.execute(batch_dataset, [trainer],
-                                       train=True, summaries=False)  # TODO same as above
+               # else:
+                #    tf_manager.execute(batch_dataset, [trainer],
+                 #                      train=True, summaries=False)  # TODO same as above
 
                 if step % validation_period == validation_period - 1:
                     val_results, val_outputs = run_on_dataset(

neuralmonkey/trainers/generic_bandit_trainer.py
@@ -3,10 +3,13 @@ import re
 
 import tensorflow as tf
 from tensorflow.python.ops import variables
+from neuralmonkey.logging import log
 
 
 from neuralmonkey.runners.base_runner import (collect_encoders, Executable,
-                                              BanditExecutionResult, NextExecute)
+                                              BanditExecutionResult,
+                                              ExecutionResult,
+                                              NextExecute)
 
 # tests: lint, mypy
 
@@ -15,15 +18,15 @@ Gradients = List[Tuple[tf.Tensor, tf.Variable]]
 BanditObjective = NamedTuple('BanditObjective',
                        [('name', str),
                         ('decoder', Any),
-                        ('loss', tf.Tensor),
-                        ('gradients', Gradients),
+                        ('samples', Any),  # TODO better type
+                        ('grad_nondiff', Gradients),
+                        ('grad_diff', Gradients),
                         # must have gradients because
                         # loss might not be fully differentiable
                         ('sample_size', int),
                         ('clip_prob', Optional[float])])
 
 BIAS_REGEX = re.compile(r'[Bb]ias')
-
 
 # pylint: disable=too-few-public-methods,too-many-locals
 class GenericBanditTrainer(object):
@@ -38,12 +41,14 @@ class GenericBanditTrainer(object):
     # FIXME
     # only one objective for now
 
-    def __init__(self, objective: BanditObjective, gradients=None,
+    def __init__(self, objective: BanditObjective,
                  l1_weight=0.0, l2_weight=0.0, learning_rate=1e-4,
                  clip_norm=False, optimizer=None) -> None:
 
-        self.optimizer = optimizer(learning_rate=learning_rate) or \
-                         tf.train.AdamOptimizer(learning_rate=learning_rate)
+        self.optimizer = optimizer or tf.train.AdamOptimizer(learning_rate=learning_rate)
+
+            #optimizer(learning_rate=learning_rate) or \
+            #             tf.train.AdamOptimizer(learning_rate=learning_rate)
 
         with tf.variable_scope('regularization'):
             regularizable = [v for v in tf.trainable_variables()
@@ -54,27 +59,43 @@ class GenericBanditTrainer(object):
             l2_value = sum(tf.reduce_sum(v ** 2) for v in regularizable)
             l2_cost = l2_weight * l2_value if l2_weight > 0 else 0.0
 
+        self.regularizer_cost = l1_cost + l2_cost
         tf.scalar_summary('train_l1', l1_value, collections=["summary_train"])
         tf.scalar_summary('train_l2', l2_value, collections=["summary_train"])
 
         # part of the gradients which is differentiable
-        # TODO which parameters?
-        self.partial_gradients = tf.gradients(objective.grad_diff,
-                                              variables.trainable_variables())
+        # gradients for whole batch
+        log("grad_diff")
+        log(objective.grad_diff)  # max_len length list of batch_size x 1 tensors
+        log(len(objective.grad_diff))
 
-        self.regularizer_cost = _sum_gradients([self._get_gradients(l)
+        # sum log probs over time steps
+        grad_sum = tf.add_n(objective.grad_diff)
+        log("grad_sum")
+        log(grad_sum)  # batch_size x sample_size (1)
+
+        grad_scaled = tf.mul(objective.grad_nondiff, grad_sum)
+        log("grad_scaled")
+        log(grad_scaled)  # batch_size x sample_size (1)
+
+        # average over batch
+        grad_avg = tf.reduce_mean(grad_scaled, 0)
+
+        # partial gradients for full sequence
+        loss_gradients = self._get_gradients(grad_avg)
+
+        # add gradients for regularization
+        regularizer_gradients = _sum_gradients([self._get_gradients(l)
                               for l in [l1_cost, l2_cost] if l != 0.])
+        self.gradients = _sum_gradients([regularizer_gradients, loss_gradients])
+        log(self.gradients)
 
-        self.all_coders = set.union(*(collect_encoders(objective.decoder)))
+        self.loss = grad_avg
+        self.all_coders = set.union(collect_encoders(objective.decoder))
 
-        self.gradients = gradients
+        self.clip_norm = clip_norm
 
-        if clip_norm:
-            assert clip_norm > 0.0
-            self.gradients = [(tf.clip_by_norm(grad, clip_norm), var)
-                         for grad, var in self.gradients]
-
-        self.sample_op = (objective.samples, self.partial_gradients)  # TODO
+        self.sample_op = objective.samples
         self.update_op = self.optimizer.apply_gradients(self.gradients)  # TODO
 
         for grad, var in self.gradients:
@@ -89,21 +110,20 @@ class GenericBanditTrainer(object):
 
     # only use for computing regularizers
     def _get_gradients(self, tensor: tf.Tensor) -> Gradients:
-        gradient_list = self.optimizer.compute_gradients(tensor)
+        gradient_list = self.optimizer.compute_gradients(
+            tensor, tf.trainable_variables())
         return gradient_list
-
-    # TODO is input really a tensor? or a dictionary?
-    def _set_gradients(self, tensor: tf.Tensor) -> Gradients:
-        self.gradients = tensor
 
     # pylint: disable=unused-argument
     def get_executable(self, train=False, update=False, summaries=True) -> Executable:
-        if update == True:
+        if update:
+            log("Update bandit")
             return UpdateBanditExecutable(self.all_coders,
-                                   self.update_op,
+                                   self.update_op, self.loss,
                                    self.scalar_summaries if summaries else None,
                                    self.histogram_summaries if summaries else None)
         else:
+            log("Sample bandit")
             return SampleBanditExecutable(self.all_coders,
                                          self.sample_op,
                                          self.regularizer_cost,
@@ -126,18 +146,22 @@ def _sum_gradients(gradients_list: List[Gradients]) -> Gradients:
 def _clip_log_probs(log_probs, prob_threshold):
     """ Clip log probabilities to some threshold """
     # threshold is prob, input are log probs
-    log_threshold = tf.log(prob_threshold)
-    log_max_value = tf.log(1)
-    return tf.clip_by_value(log_probs, clip_value_min=log_threshold,
+    if prob_threshold > 0.00:
+        log_threshold = tf.log(prob_threshold)
+        log_max_value = tf.log(1)
+        return tf.clip_by_value(log_probs, clip_value_min=log_threshold,
                             clip_value_max=log_max_value)
+    else:
+        return log_probs
 
 
 class UpdateBanditExecutable(Executable):
 
-    def __init__(self, all_coders, update_op, scalar_summaries,
+    def __init__(self, all_coders, update_op, losses, scalar_summaries,
                  histogram_summaries):
         self.all_coders = all_coders
         self.update_op = update_op
+        self.losses = losses
         self.scalar_summaries = scalar_summaries
         self.histogram_summaries = histogram_summaries
 
@@ -148,7 +172,7 @@ class UpdateBanditExecutable(Executable):
         if self.scalar_summaries is not None:
             fetches['scalar_summaries'] = self.scalar_summaries
             fetches['histogram_summaries'] = self.histogram_summaries
-
+        fetches['losses'] = self.losses
         return self.all_coders, fetches, {}
 
     def collect_results(self, results: List[Dict]) -> None:
@@ -160,8 +184,15 @@ class UpdateBanditExecutable(Executable):
             scalar_summaries = results[0]['scalar_summaries']
             histogram_summaries = results[0]['histogram_summaries']
 
-        self.result = BanditExecutionResult(
-            [], scalar_summaries=scalar_summaries,
+        losses_sum = [0. for _ in self.losses]
+        for session_result in results:
+            for i in range(len(self.losses)):
+                # from the end, losses are last ones
+                losses_sum[i] += session_result['losses'][i]
+        avg_losses = [s / len(results) for s in losses_sum]
+
+        self.result = ExecutionResult(
+            [], losses=avg_losses, scalar_summaries=scalar_summaries,
             histogram_summaries=histogram_summaries,
             image_summaries=None)
 
@@ -196,8 +227,10 @@ class SampleBanditExecutable(Executable):
             scalar_summaries = results[0]['scalar_summaries']
             histogram_summaries = results[0]['histogram_summaries']
 
+        sampled_outputs = self.sample_op
+        outputs = sampled_outputs, self.regularization_cost
         self.result = BanditExecutionResult(
-            [],
+            [outputs],  # TODO
             scalar_summaries=scalar_summaries,
             histogram_summaries=histogram_summaries,
             image_summaries=None)