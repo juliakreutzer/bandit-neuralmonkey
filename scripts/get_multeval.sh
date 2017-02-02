caff839387dd681e1545ca548c13449de99eb1a1
Julia Kreutzer <juliakreutzer1@aol.com>
2017-02-02 01:42:26 +0100

    fixed meteor script

--- scripts/get_multeval.sh
+++ scripts/get_multeval.sh
@@ -7,8 +7,10 @@ tar -xvzf multeval-0.5.1.tgz
 
 # run an example to download METEOR
 
-./multeval.sh eval --refs example/refs.test2010.lc.tok.en.* \
-                   --hyps-baseline example/hyps.lc.tok.en.baseline.opt* \
+cp multeval-0.5.1/constants ../
+
+
+multeval-0.5.1/multeval.sh eval --refs multeval-0.5.1/example/refs.test2010.lc.tok.en.* \
+                   --hyps-baseline multeval-0.5.1/example/hyps.lc.tok.en.baseline.opt* \
                    --meteor.language en
 
-cp multeval-0.5.1/constants ../
