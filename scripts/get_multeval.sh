48ac364e2a96c4358e5a5ca771799d3a8f15bd94
Julia Kreutzer <juliakreutzer1@aol.com>
2017-02-14 19:00:05 +0100

    removed multeval dummy run

--- scripts/get_multeval.sh
+++ scripts/get_multeval.sh
@@ -5,12 +5,4 @@
 wget http://www.cs.cmu.edu/~jhclark/downloads/multeval-0.5.1.tgz
 tar -xvzf multeval-0.5.1.tgz
 
-# run an example to download METEOR
-
 cp multeval-0.5.1/constants ../
-
-
-multeval-0.5.1/multeval.sh eval --refs multeval-0.5.1/example/refs.test2010.lc.tok.en.* \
-                   --hyps-baseline multeval-0.5.1/example/hyps.lc.tok.en.baseline.opt* \
-                   --meteor.language en
-
